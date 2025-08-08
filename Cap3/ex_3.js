01e4165f528128593da2acd3604138a036574dd3
Jaosuzart <joaomarcelosuzartcastro@gmail.com>
2025-08-08 15:25:55 -0300

    Enviando ataulizacao ao github

--- Cap3/ex_3.js
+++
@@ -1,7 +0,0 @@
-const prompt = require("prompt-sync")(); // adiciona o pacote ao programa
-const salario = Number(prompt("Salário R$: ")); // lê os dados de entrada
-const tempo = Number(prompt("Tempo (anos): "));
-const quadrienios = Math.floor(tempo / 4); // calcula valor da entrada
-const acrescimo = (salario * quadrienios) / 100;
-console.log(`Quadriênios: ${quadrienios}`);
-console.log(`Salário Final R$: ${(salario + acrescimo).toFixed(2)}`);
